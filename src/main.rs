21a010761fbd6cfba958209b152832eb92d4982d
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2024-04-08 09:24:10 +0200

    create basic proxy system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 mod hteapot;
 mod config;
-use std::fs;
+use std::{fs, io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 use hteapot::HteaPot;
 
 use crate::hteapot::HttpStatus;
+
+
+fn fetch(url: &str) -> String {
+    let url = url.trim_start_matches("http://");
+    let mut client = TcpStream::connect(url).unwrap();
+    let http_request = format!("GET / HTTP/1.1\r\nHost: {}\r\n\r\n", url);
+    client.write(http_request.as_bytes()).unwrap();
+    let mut response = String::new();
+    let mut buffer = [0; 1024];
+    loop {
+        match client.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => {
+                response.push_str(std::str::from_utf8(&buffer[..n]).unwrap());
+                println!("{}: {}", n,response);
+                if response.ends_with("\n") {break} //TODO: break when size == header
+                
+            },
+            Err(_) => break
+        }
+    }
+    response
+}
 
 
 fn main() {
@@ -25,6 +48,12 @@ fn main() {
         } else {
             req.path.clone()
         };
+        if config.proxyRules.contains_key(&req.path) {
+            println!("Proxying to: {}", config.proxyRules.get(&req.path).unwrap());
+            let url = config.proxyRules.get(&req.path).unwrap();
+            return fetch(url);
+            //return HteaPot::response_maker(HttpStatus::OK, "", Some(url.to_string()));
+        }
         let path = format!("./{}/{}",config.root, path);
         let content = fs::read_to_string(path);
         match content {
