9196d9c15fd608e908de3d1ca6e4bbfda0030f9e
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2025-04-09 10:15:13 +0200

    move docs to the wikie

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,7 +2,7 @@ use hteapot::{Hteapot, HttpRequest, HttpResponse, HttpStatus};
 
 fn main() {
     let server = Hteapot::new("localhost", 8081);
-    server.listen(move |req: HttpRequest| {
+    server.listen(move |_req: HttpRequest| {
         // This will be executed for each request
         HttpResponse::new(HttpStatus::IAmATeapot, "Hello, I am HTeaPot", None)
     });
