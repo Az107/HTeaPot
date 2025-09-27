3c581ef0a571f16b4381ec56208c8747f1b3ae85
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2025-09-27 20:33:26 +0200

    minor fixes and windows specific fixes

--- src/handler/cgi.rs
+++ src/handler/cgi.rs
@@ -4,9 +4,9 @@ use crate::hteapot::{HttpRequest, HttpStatus};
 
 #[cfg(feature = "cgi")]
 pub fn serve_cgi(
-    program: &String,
-    file_dir: &String,
-    file_name: &String,
+    program: String,
+    file_dir: String,
+    file_name: String,
     request: HttpRequest,
 ) -> Result<(HttpStatus, HashMap<String, String>, Vec<u8>), &'static str> {
     use std::{
@@ -14,6 +14,15 @@ pub fn serve_cgi(
         io::Write,
         process::{Command, Stdio},
     };
+    //THIS LINES ONLY EXIST COS WINDOWS >:C
+    let file_dir = if file_dir.starts_with("\\\\?\\") {
+        file_dir.strip_prefix("\\\\?\\").unwrap().to_string()
+    } else {
+        file_dir.to_owned()
+    };
+    let file_dir = file_dir.replace("\\", "/");
+    let file_name = file_name.replace("\\", "/");
+
     let query = request
         .args
         .iter()
@@ -29,7 +38,7 @@ pub fn serve_cgi(
     unsafe {
         //TODO: !! fix this, avoid using unsafe , this could conflict simultaneous CGI executions, change to fastCGI ?
         env::set_var("REDIRECT_STATUS", "hteapot");
-        // 1. Información del script y request
+
         env::set_var("GATEWAY_INTERFACE", "CGI/1.1");
         env::set_var("SERVER_PROTOCOL", "HTTP/1.1"); // ej. "HTTP/1.1"
         env::set_var("REQUEST_METHOD", request.method.to_str());
@@ -42,20 +51,20 @@ pub fn serve_cgi(
 
         // SCRIPT_NAME = ruta relativa al docroot
         // SCRIPT_FILENAME = ruta absoluta al script en disco
-        env::set_var("SCRIPT_NAME", &request.path);
-        env::set_var("SCRIPT_FILENAME", format!("{}{}", &file_dir, &request.path));
+        env::set_var("SCRIPT_NAME", &file_name);
+        env::set_var("SCRIPT_FILENAME", format!("{}{}", &file_dir, &file_name));
 
         // PATH_INFO es opcional, solo si usas /index.php/loquesea
         env::set_var(
             "PATH_INFO",
-            &request.path.strip_prefix(file_name).unwrap_or("/"),
+            request.path.strip_prefix(&file_name).unwrap_or("/"),
         );
 
         // Server info
         env::set_var("SERVER_NAME", "localhost"); // ej. "localhost" change to get from config
         env::set_var("SERVER_PORT", "8081"); // ej. "8080"
         env::set_var("HTTP_HOST", "localhost:8081"); // ej. "8080"
-        env::set_var("SERVER_SOFTWARE", "hteapot/0.6.2");
+        env::set_var("SERVER_SOFTWARE", "hteapot/0.6.5");
         env::set_var("REMOTE_ADDR", "localhost"); // this should obtain the real address ?
     }
 
